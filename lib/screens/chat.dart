9f54e52557cbea1a51753fd7beaf8f4c55ce9360
hoosamMr <hoosam.mr@gmail.com>
2024-01-24 00:26:00 +0200

    adding signOut function

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class ChatScreen extends StatelessWidget {
@@ -9,6 +10,17 @@ class ChatScreen extends StatelessWidget {
         title: const Text(
           'Flutter Chat',
         ),
+        actions: [
+          IconButton(
+            onPressed: () {
+              FirebaseAuth.instance.signOut();
+            },
+            icon: Icon(
+              Icons.exit_to_app,
+              color: Theme.of(context).colorScheme.primary,
+            ),
+          ),
+        ],
       ),
       body: const Center(
         child: Text(
